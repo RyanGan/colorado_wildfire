4c3ec00be8953fed39b3487f51975d6f3e5f2222
AprilJing <ljy199341@163.com>
2017-02-02 11:49:58 -0700

    Not sure...start try

---
+++ r_scripts/proportion_intersection_zip_wrfgrid_co.r
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------------------------
+# Title: Proportion Intersection Zip WRFGrid CO
+# Author: Jingyang Liu
+# Date Created: 1/30/17
+# R version: 3.3.2
+# ------------------------------------------------------------------------------
+
+library(rgdal) # package for shape files
+library(sp)
+library(rgeos) # rgeos package contains the intersect and area commands I need
+library(tidyverse)
+
+grid_dir <- paste0('./instructions/co_grid_shp_nad83')
+
+smoke_grid <- readOGR(dsn = grid_dir, layer = "co_grid") # ?
+summary(smoke_grid) # has 1107 here
+plot(smoke_grid) 
+
+getwd()
+
+
+
+
