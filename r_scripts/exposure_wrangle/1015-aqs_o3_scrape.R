0519afef1e9d09408683c67f2c7434477265aba6
RyanGan <ryan.william.gan@gmail.com>
2018-11-05 13:10:30 -0500

    Finished kriging; may need to revisit to add performance metrics.

--- r_scripts/exposure_wrangle/1015-aqs_o3_scrape.R
+++ r_scripts/exposure_wrangle/1015-aqs_o3_scrape.R
@@ -84,3 +84,116 @@ rep('c', 25)
 read.csv('https://aqs.epa.gov/api/rawData?user=ryan.william.gan@gmail.com&pw=aquamouse29&format=DMCSV&param=44201&state=08&county=001&bdate=20100101&edate=20100101')
 
 rm(data)
+
+# move study map chunk to other script -----------------------------------------
+# study map so I can visualize where to make this cut
+# clip by bbox function ------
+bbox_clip <- function(sf, bbox) {
+  # find the CRS of the sf object
+  crs <- sf::st_crs(sf)$proj4string
+  # create matrix
+  x <- c(bbox[1], bbox[1], bbox[3], bbox[3], bbox[1])
+  y <- c(bbox[2], bbox[4], bbox[4], bbox[2], bbox[2])
+  coords <- matrix(cbind(x, y), ncol=2)
+  # create polygon and assign same coord crs as sf object
+  coords_poly <- sp::Polygon(coords)
+  bbox_poly <- sp:: SpatialPolygons(list(sp::Polygons(list(coords_poly),
+                                                      ID = "bbox")), proj4string = sp::CRS(crs))
+  # convert to sf feature
+  bbox_sf <- st_as_sf(bbox_poly)
+  # clip sf object
+  clipped_sf <- sf[bbox_sf,]
+  return(clipped_sf)
+}
+
+# clipping bounding box
+study_bbox <- st_bbox(c(xmin=-105.3, xmax=-104.5, ymax=41, ymin = 38))
+
+# county subset
+county_sub_name <- c("Larimer", "Weld", "Boulder", "Broomfield", "Adams", 
+                     "Denver", "Jefferson", "Arapahoe", "Douglas", "El Paso",
+                     "Pueblo")
+
+# read in county shapefile and subset to only colorado fips
+co_county_sf <- st_read("../meta_wildfire/data/shapefile/us_county", 
+                        layer = "us_county") %>%
+  # limit to colorado
+  filter(STATEFP == "08") %>% 
+  # create fips variable
+  mutate(fips = paste0(STATEFP, COUNTYFP))
+
+# save wgs84 crs
+wgs <- st_crs(co_county_sf)
+
+# county clip
+county_clip <- bbox_clip(co_county_sf, study_bbox)
+
+# extract county names
+county_text <- county_clip %>% 
+  st_transform(wgs) %>% 
+  st_centroid() %>% 
+  st_coordinates() %>% 
+  cbind(., as.character(county_clip$NAME)) %>%
+  as_data_frame() %>% 
+  rename(lon = X, lat = Y, county = V1) %>% 
+  mutate(lon = as.numeric(lon), lat = as.numeric(lat))
+
+
+# read in city polygons 2010
+city <- st_read("./data/shapefiles/Colorado_City_Point_Locations/", 
+                layer = "Colorado_City_Point_Locations") %>% 
+  st_transform(crs = wgs)
+
+# limit cities 
+city_points <- city %>% 
+  filter(NAME %in% c("FORT COLLINS", "PUEBLO", "GREELEY", "BOULDER", "DENVER",
+                     "COLORADO SPRINGS")) %>% 
+  mutate(city = stringr::str_to_title(NAME))
+
+
+# read colorado 2015 population raster
+co_pop_2015 <- raster("./data/shapefiles/2015-ColoradoPopDensity.tif")
+# extract bbox of clipped county subset
+fr_extent <- extent(st_bbox(county_clip)[c(1,3,2,4)])
+# raster
+fr_pop_2015 <- crop(co_pop_2015, fr_extent)
+
+# i'm going to create a shapefile/simple features; easier to plot
+front_range_pop_sf <- st_as_sf(rasterToPolygons(fr_pop_2015)) %>% 
+  rename(popden = X2015.ColoradoPopDensity) %>% 
+  # filtering to cells > 100 
+  filter(popden > 100)
+
+# cut once more to county shapefile so that populations outside the counties are not present
+pop_clip <- front_range_pop_sf[county_clip,]
+
+# plot map
+study_map <-ggplot(pop_clip) +
+  # start with plot of simple features of populations
+  geom_sf(aes(fill=popden), color = NA) +
+  # define aesthetics of poipulation
+  scale_fill_gradient(name = expression("Population per km"^2), 
+                      low = "#26d0ce", high = "#1a2980") +
+  # plot lines of counties
+  geom_sf(data = county_clip, color = "black", 
+          fill = "transparent", size = 0.5) +
+  geom_point(data = city_points, aes(x = LONG, y = LAT), color = "#3f2b96") +
+  geom_text(data = city_points, aes(x = LONG, y = LAT, label = city), 
+            color = "#3f2b96", size = 4, hjust = 1, vjust = -0.6) +
+  # stations
+  geom_point(data = monitor_locs, aes(x=SITE_LONGITUDE, y=SITE_LATITUDE), 
+             color = 'red', size = 0.5) +
+  # grid points
+  geom_point(data=grid, aes(x=lon, y=lat), color = 'black', size = 0.5) +
+  # theme
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_line(colour = 'transparent'),
+        panel.grid.minor = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        legend.key = element_rect(fill = NA, colour = NA, size = 0.25))
+
+study_map
+
+# --------
--- r_scripts/exposure_wrangle/1015-colorado_o3_krig.R
+++ r_scripts/exposure_wrangle/1015-colorado_o3_krig.R
@@ -1,17 +1,125 @@
-# Adapted from Sheena's code
+# ------------------------------------------------------------------------------
+# Title: Krig of ozone from EPA monitors to Colorado front range grid
+# Author: Ryan Gan
+# Date Created: 2018-10-31
+# ------------------------------------------------------------------------------
+
+# Introduction: This code is adapted from Sheena's code
+# https://github.com/smartenies/AQS_Data_Scrape/blob/master/R/03_Kriging_AQS_Data.R
+# I am using prepared 8 hour max ozone estimates from AQS since the API for raw
+# AQS was down at the time.
 
 # Libraries
-library(tidyverse)
+library(tidyverse) # basic data management
+library(gstat) # used for ordinary krig
+library(sp) # spatial objects
+library(sf)
 
-# looks like Sheena runs on one machine?
-# I wonder if I can run the scrape and interpolation in parallel on ozone?
-# There are some limitations on the EPA AQS AQI. Limited to 1 year and 1 county
-# in 1 state. I'd like to use some version of apply rather than a for loop. 
-# First step will be to construct a list of URLs to grab.
+# Vector of files to read
+files <- paste0('./data/smoke/', seq(2010,2015), '-co_daily_ozone.csv')
 
-# AQS base url that contains my user name and password for AQS site; I will
-# probably hide before I push to github
+# Reading in 2010 to 2015 ozone data
+ozone <- map_dfr(files, function(x) {
+  read_csv(x) %>% 
+  rename(o3_8hr_max = `Daily Max 8-hour Ozone Concentration`)
+})
+
+# Find Colorado monitor locations; taking only first obs of each monitor
+monitor_locs <- ozone %>% 
+  group_by(AQS_SITE_ID) %>% 
+  filter(row_number()==1) %>% 
+  filter(POC == 1) %>% 
+  # filter grid coords to front range bounding box
+  filter(SITE_LONGITUDE >= -105.42 & SITE_LONGITUDE <= -104.452) %>% 
+  filter(SITE_LATITUDE >= 38 & SITE_LATITUDE <= 41)
+
+# read in 15x15 grid
+study_grid <- read_csv('./data/smoke/krig_grid_id_coords.csv') %>% 
+  # filter grid coords to front range bounding box
+  filter(lon >= -105.42 & lon <= -104.452) %>% 
+  filter(lat >= 38.5 & lat <= 40.75) %>% 
+  filter(!(GRID_ID %in% c(23715, 23716, 23717)))
+
+# saving study grid
+#write_csv(study_grid, path = './data/smoke/front_range_grid.csv')
+
+# Note: I believe the Krig grid should be larger
+  ggplot() +
+  geom_point(data = monitor_locs, aes(x=SITE_LONGITUDE, y=SITE_LATITUDE), 
+             color = 'red', size = 0.5) +
+  # grid points
+  geom_point(data=study_grid, aes(x=lon, y=lat), color = 'black', size = 0.5) 
 
 
+# limiting to front range ozone monitors
+fr_o3 <- ozone %>% 
+  filter(AQS_SITE_ID %in% monitor_locs$AQS_SITE_ID) %>% 
+  mutate(Date = as.Date(Date, format = '%m/%d/%Y')) %>% 
+  rename(lat = SITE_LATITUDE, lon = SITE_LONGITUDE)
 
+# converting front range grid to spatial points dataframe
+coordinates(study_grid) = ~lon+lat
 
+# vector of grid ids
+grid_id_vector <- study_grid@data$GRID_ID
+
+# create vector of dates to map through
+date_vector <- unique(fr_o3$Date)
+
+# define function to perform krig on varying monitors for each day
+# returns vector of predictions for each grid cell 
+krige_fun <- function(filter_date){
+  # subset ozone data
+  o3_subset <- filter(fr_o3, Date == filter_date) 
+  
+  # convert o3_subset to a spatial points dataframe
+  coordinates(o3_subset) = ~lon+lat
+  
+  # set up variogram
+  all_models <- c("Exp", "Sph", "Gau", "Cir", "Lin", "Log")
+
+  # cutoff distance is 40 km based on what sheena used
+  c_dist = 40000
+
+  # define variogram
+  vgm <- variogram(o3_8hr_max ~ 1, data = o3_subset)
+
+  # fit variogram
+  vgm_fit <- fit.variogram(vgm, model=vgm(all_models),
+                           fit.kappa = seq(0.3,5,0.1))
+
+  # fit model
+  model <- as.character(vgm_fit$model)[nrow(vgm_fit)]
+  
+  # ordinary kriging model  
+  krige_result <- krige(o3_8hr_max ~ 1, o3_subset, study_grid, vgm_fit)
+
+  # leave-one out crossvalidation for validation metrics
+  # cross validated
+  cv_krige_result <- krige.cv(o3_8hr_max ~ 1, o3_subset, vgm_fit, nfold = 10)
+  # cross validated
+  cv_correlation <- cor(cv_krige_result$var1.pred, 
+                        cv_krige_result$observed, 
+                        method = 'pearson')
+  # return kriging predictions for each grid cell
+  krige_pred <- data.frame(krige_result$var1.pred) 
+  colnames(krige_pred) <- paste0('d_', filter_date)
+  return(krige_pred)
+}
+
+# start time
+start <- Sys.time()
+
+# map kriging over date vector
+kriged_ozone <- map_dfc(date_vector, ~ krige_fun(.x)) 
+
+# end time
+end <-  Sys.time() - start
+end
+
+# bind grid ids
+kriged_ozone <- cbind(grid_id_vector, kriged_ozone)
+summary(kriged_ozone[,1:20])
+
+# write kriged estimates; I should probably consider some performance metrics
+write_csv(kriged_ozone, path = './data/smoke/1015-frontrange_kriged_o3')
